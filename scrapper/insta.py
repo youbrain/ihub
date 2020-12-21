ace939c4e866d1dfc07163a7a37fb3444aa8abca
Alexashka <647533sancho@gmail.com>
2020-12-21 13:47:07 +0200

    instagram parser finished

--- scrapper/insta.py
+++ scrapper/insta.py
@@ -1,67 +1,50 @@
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 import requests
-# import json
-from bs4 import BeautifulSoup as bs4
+import json
 
-import pickle
-from time import sleep
 
-config = {
-    'insta_url': 'https://www.instagram.com/ihub_khm/tagged/'
+cookies = {
+    "ig_did": "58971998-1837-4A06-B2DA-2DBEAB6B2250",
+    "mid": "XrujiAALAAHHAKk-gpcHvE3x4TMV",
+    "ds_user_id": "5961078248",
+    "csrftoken": "mmc2td6kSwrZZzNMsu0F8fLCntQCxqLL",
+    "sessionid": "5961078248%3AOLUa6tDvgbHjJz%3A15",
 }
 
 
-def get_insta_posts():
-    cookies = {
-        "ig_did": "58971998-1837-4A06-B2DA-2DBEAB6B2250",
-        "mid": "XrujiAALAAHHAKk-gpcHvE3x4TMV",
-        "ds_user_id": "5961078248",
-        "csrftoken": "mmc2td6kSwrZZzNMsu0F8fLCntQCxqLL",
-        "sessionid": "5961078248%3AOLUa6tDvgbHjJz%3A15",
-    }
-    '''
-    driver = webdriver.Chrome()
-    driver.get('https://www.instagram.com/ihub_khm/tagged/')
+class InstaParser:
+    def __init__(self, cookies):
+        self.session = requests.session()
+        self.cookies = cookies
 
-    for cookie in cookies.items():
-        driver.add_cookie(
-            {"name": cookie[0], "value": cookie[1], 'domain': '.instagram.com'})
+    def get_profil_id(self, username):
+        r = self.session.get(
+            'https://www.instagram.com/' + username + '/', cookies=self.cookies)
+        return r.text.split('profilePage_')[1].split('"')[0]
 
-    driver.refresh()
+    def get_tagged_posts(self, profil_id):
+        r = self.session.get(
+            'https://www.instagram.com/graphql/query/' +
+            '?query_hash=31fe64d9463cbbe58319dced405c6206&variables={"id":' +
+            profil_id + ',"first":9999}',
+            cookies=self.cookies
+        )
 
-    element = WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "_bz0w"))
-    )
-
-    page = bs4(driver.page_source)
-
-    links = []
-    for link in page.findAll('a'):
-        link = link.get('href')
-        if link.startswith('/p/'):
-            links.append(link)
-    '''
-    links = ['/p/CIlPczyMKqp/',
-             '/p/CIlIz8eosb7/',
-             '/p/CITVAnvszSY/',
-             '/p/CISvfrSotpD/',
-             '/p/CILCeSgMFPF/',
-             '/p/CIG4mHkFaUg/',
-             '/p/CH_DGBjMIeB/',
-             '/p/CH46lhOnZ3z/',
-             '/p/CHaIxiRs8Rs/',
-             '/p/CHaFchpsc0D/',
-             '/p/CHZ21YfMRkq/',
-             '/p/CHYYOh5M7yv/']
-
-    for link in links:
-        r = requests.get('https://www.instagram.com' + link, cookies=cookies)
-        desc = r.text.split(
-            ':{"edges":[{"node":{"text":"')[1].split(r'"}}]}')[0]
+        r_json = json.loads(r.text)
+        posts = r_json['data']['user']['edge_user_to_photos_of_you']['edges']
+        return posts
 
 
 if __name__ == '__main__':
-    print(get_insta_posts())
+    parser = InstaParser(cookies)
+    hub_id = parser.get_profil_id('ihub_khm')
+    posts = parser.get_tagged_posts(hub_id)
+
+    print(
+'https://www.instagram.com/p/' + posts[0]['node']['shortcode'],      # link
+posts[0]['node']['taken_at_timestamp'], # published time
+posts[0]['node']['owner']['username'], # owner username
+posts[0]['node']['edge_media_to_caption']['edges'][0]['node']['text'], # description
+posts[0]['node']['display_url'], # photo link
+posts[0]['node']['edge_media_to_comment']['count'], # cooments count
+posts[0]['node']['edge_liked_by']['count'], # likes count
+    )
